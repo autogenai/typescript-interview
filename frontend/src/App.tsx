03b4a7462cd4858b372a4ef580430a205735fc80
Mike Walker <michael.walker@softwire.com>
2023-08-30 14:11:20 +0100

    Fix formatting.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,20 @@
-import { useState } from 'react'
-import { SummariseForm } from './SummariseForm'
+import { useState } from "react";
+import { SummariseForm } from "./SummariseForm";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <div className="bg-neutral-100 min-h-screen">
-      <main className="max-w-[1200px] m-auto pt-4">
-        <h1 className="text-4xl font-light mb-6">AutogenAI Interview</h1>
-        <div className="flex">
-          <SummariseForm />
-          <div>TODO</div>
+    return (
+        <div className="bg-neutral-100 min-h-screen">
+            <main className="max-w-[1200px] m-auto pt-4">
+                <h1 className="text-4xl font-light mb-6">AutogenAI Interview</h1>
+                <div className="flex">
+                    <SummariseForm />
+                    <div>TODO</div>
+                </div>
+            </main>
         </div>
-      </main>
-    </div>
-  )
+    );
 }
 
-export default App
+export default App;
--- frontend/src/SummariseForm.tsx
+++ frontend/src/SummariseForm.tsx
@@ -6,13 +6,15 @@ export function SummariseForm() {
         event.preventDefault();
     }
 
-    return (          
+    return (
         <form className="mr-4 w-1/2 bg-white rounded-md border border-neutral-200 p-4" onSubmit={handleSubmit}>
             <label className="block mb-4">
                 <span className="block font-medium mb-4">Text to summarise</span>
-                <textarea className="border border-neutral-400 p-4 w-full rounded-md h-[360px] resize-none"/>
+                <textarea className="border border-neutral-400 p-4 w-full rounded-md h-[360px] resize-none" />
             </label>
-            <button type="submit" className="p-3 bg-neutral-800 rounded-md text-white font-medium hover:bg-neutral-900">Summarise</button>
+            <button type="submit" className="p-3 bg-neutral-800 rounded-md text-white font-medium hover:bg-neutral-900">
+                Summarise
+            </button>
         </form>
-    )
+    );
 }
